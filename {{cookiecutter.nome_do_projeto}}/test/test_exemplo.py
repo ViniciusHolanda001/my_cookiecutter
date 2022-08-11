9910864df111b0972a291a37416885cb6c9cede4
Vinicius Holanda Cavalcante <viniciusholandacavalcante@gmail.com>
2022-08-10 21:04:06 -0300

    Pytest

--- {{cookiecutter.nome_do_projeto}}/test/test_exemplo.py
+++ {{cookiecutter.nome_do_projeto}}/test/test_exemplo.py
@@ -1,17 +1,13 @@
-import unittest
+import pytest
 
 # Importar as funcoes que vao ser testadas
 # from src.nome_do_arquivo import nome_da_funcao
 
 
-class Test{{cookiecutter.nome_do_projeto}}(unittest.TestCase):
+class Test():
 
     def test_nome_da_funcao(self):
 
         # variavel = ''
         # resultado = ''
-        # self.assertEqual(nome_da_funcao(variavel), resultado)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        return ''
